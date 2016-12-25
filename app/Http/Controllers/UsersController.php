398d9813f4b78fea8426e4e3a24d15988dd2d2c4
xuzhiwei <jsrdxzw@163.com>
2016-12-25 10:47:19 +0800

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -11,5 +11,21 @@ class UsersController extends Controller
 {
     public function create(){
         return view('users.create');
+    }
+
+    public function show($id){
+        $user = User::findOrFail($id);
+        return view('users.show',compact('user'));
+    }
+
+    public function store(Request $request){
+        $this->validate($request, [
+        'name' => 'required|max:50',
+        'email' => 'required|email|unique:users|max:255',
+        'password' => 'required'
+    ]);
+      $user = User::create(['name'=>$request->name,'email'=>$request->email,'password'=>$request->password]);
+      session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+      return redirect()->route('users.show', [$user]);
     }
 }
